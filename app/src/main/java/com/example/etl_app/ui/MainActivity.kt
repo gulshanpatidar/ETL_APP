da3e704076dc4cb481e80b91ca8e766f3762af15
gulshan <jankarikibaat@gmail.com>
2021-09-22 16:35:54 +0530

    added login ui.

--- app/src/main/java/com/example/etl_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/etl_app/ui/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.etl_app.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
@@ -11,19 +10,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
-import com.example.etl_app.R
 import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MdcTheme() {
-                Greeting()
+            MdcTheme {
+                LoginHome()
             }
         }
     }
